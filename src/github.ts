0499a919687a15a0f12c8a8edd219594cdabf233
Hidetake Iwata <int128@gmail.com>
2025-05-15 11:00:01 +0900

    Remove @actions/github

---
+++ src/github.ts
@@ -0,0 +1,31 @@
+import assert from 'assert'
+
+export type Context = {
+  repo: {
+    owner: string
+    repo: string
+  }
+  eventName: string
+  sha: string
+  workflow: string
+}
+
+export const getContext = (): Context => {
+  // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+  return {
+    repo: getRepo(),
+    eventName: getEnv('GITHUB_EVENT_NAME'),
+    sha: getEnv('GITHUB_SHA'),
+    workflow: getEnv('GITHUB_WORKFLOW'),
+  }
+}
+
+const getRepo = () => {
+  const [owner, repo] = getEnv('GITHUB_REPOSITORY').split('/')
+  return { owner, repo }
+}
+
+const getEnv = (name: string): string => {
+  assert(process.env[name], `${name} is required`)
+  return process.env[name]
+}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
+import * as github from './github.js'
 import { run } from './run.js'
 
 const main = async (): Promise<void> => {
-  await run({
-    junitXmlPath: core.getInput('junit-xml-path', { required: true }),
-    metricNamePrefix: core.getInput('metric-name-prefix', { required: true }),
-    filterTestCaseSlowerThan: parseFloat(core.getInput('filter-test-case-slower-than', { required: true })),
-    sendTestCaseSuccess: core.getBooleanInput('send-test-case-success', { required: true }),
-    sendTestCaseFailure: core.getBooleanInput('send-test-case-failure', { required: true }),
-    enableMetrics: core.getBooleanInput('enable-metrics', { required: true }),
-    datadogApiKey: core.getInput('datadog-api-key'),
-    datadogSite: core.getInput('datadog-site'),
-    datadogTags: core.getMultilineInput('datadog-tags'),
-    repositoryOwner: github.context.repo.owner,
-    repositoryName: github.context.repo.repo,
-    workflowName: github.context.workflow,
-  })
+  await run(
+    {
+      junitXmlPath: core.getInput('junit-xml-path', { required: true }),
+      metricNamePrefix: core.getInput('metric-name-prefix', { required: true }),
+      filterTestCaseSlowerThan: parseFloat(core.getInput('filter-test-case-slower-than', { required: true })),
+      sendTestCaseSuccess: core.getBooleanInput('send-test-case-success', { required: true }),
+      sendTestCaseFailure: core.getBooleanInput('send-test-case-failure', { required: true }),
+      enableMetrics: core.getBooleanInput('enable-metrics', { required: true }),
+      datadogApiKey: core.getInput('datadog-api-key'),
+      datadogSite: core.getInput('datadog-site'),
+      datadogTags: core.getMultilineInput('datadog-tags'),
+    },
+    github.getContext(),
+  )
 }
 
 main().catch((e: Error) => {
